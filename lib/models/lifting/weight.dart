60469bcf3af3235f7e7acb5692ff7025bd05642b
Kevin  Yeboah <kevboah@gmail.com>
2020-05-26 17:45:52 -0400

    minor error message fixes in weight class

--- lib/models/lifting/weight.dart
+++ lib/models/lifting/weight.dart
@@ -48,10 +48,12 @@ class Weight {
     //keep adding 45 till get over weight then go back
     //keep adding 25s till get over then go back;
     //ans so on.. until hit weight
+    return null;
   }
 
   static KiloPlates calculateKiloPlates(double kilos, {double barWeight = 20}) {
     // TODO implement calc kilo plates
+    return null;
   }
 
 }
