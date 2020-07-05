157d157559e1076dbe2043e410be522acd3a4df3
Kevin  Yeboah <kevboah@gmail.com>
2020-07-05 15:40:25 -0400

    dependencies for google signin and service account

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,10 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/services.dart';
 import 'package:juma/models/users/user.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      "email",
+      "https://www.googleapis.com/auth/drive.file",
+    ]
+  );
 
   User _userFromFirebaseUser(FirebaseUser user) {
     return user == null ? null 
@@ -16,6 +23,7 @@ class AuthService {
     return _auth.onAuthStateChanged;
   }
 
+  // register with email and password
   Future<String> registerWithEmailAndPassword(String email, String password) async {
     try {
       var result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
@@ -55,7 +63,25 @@ class AuthService {
     }
   }
 
-  // register with email and password
+  // google sign in
+  Future<String> signInWithGoogle() async {
+    try {
+      GoogleSignInAccount googleAccount = await _googleSignIn.signIn();
+      if (googleAccount == null) return null;
+
+      GoogleSignInAuthentication accountCreds = await googleAccount.authentication;
+
+      var result = await _auth.signInWithCredential(
+        GoogleAuthProvider.getCredential(idToken: accountCreds.idToken, accessToken: accountCreds.accessToken)
+      );
+
+      return result.user.uid;
+    }
+    catch (e) {
+      print(e);
+      return null;
+    }
+  }
 
   // sign out
   Future signOut() async {
