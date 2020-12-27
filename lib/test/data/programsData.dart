de2d7438fe9e0bd15f5340a49e010e778b91bf91
Kevin  Yeboah <kevboah@gmail.com>
2020-12-26 19:58:30 -0500

    new test data folder

---
+++ lib/test/data/programsData.dart
@@ -0,0 +1,89 @@
+import 'package:juma/models/lifting/program.dart';
+import 'package:juma/models/users/user.dart';
+import 'package:juma/theme/Colors.dart';
+
+List<ProgramTemplate> testPrograms = 
+[
+  ProgramTemplate(
+    title: "Sheiko Cycle 5",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "Sheiko is no joke. Gain endurance and strength as well as increasing you one rep max.",
+    pathToMedia: "assets/photos/deadlift.jpg",
+    theme: ColorThemes.gold,
+    trainingBlocks: []
+  ),
+
+  ProgramTemplate(
+    title: "The Powerbuilding Program",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "Not for the faint of heart. A true PowerBuilding can handle the pressure and be the bridge between the two worlds",
+    pathToMedia: "assets/photos/myPhoto.jpg",
+    theme: ColorThemes.red,
+    trainingBlocks: []
+  ),
+
+  ProgramTemplate(
+    title: "Hurtin' Bombs",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "Let's hurt some bombs BABY!",
+    pathToMedia: "assets/photos/russ.jpeg",
+    theme: ColorThemes.purple,
+    trainingBlocks: []
+  ),
+  
+  ProgramTemplate(
+    title: "The Monolith",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "One goal and one goal only: Become the monolith of strength.",
+    pathToMedia: "assets/photos/russ.png",
+    theme: ColorThemes.purple,
+    trainingBlocks: []
+  ),
+
+  ProgramTemplate(
+    title: "Five By Five",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "For the beginner lifter. Skyrocket your training by starting off on the right foot.",
+    pathToMedia: "assets/photos/sageNaruto.jpg",
+    trainingBlocks: []
+  ),
+
+  ProgramTemplate(
+    title: "Flexx Fire",
+    author: UserIdentifier(
+      uid: "mLSHdKz6Engo1bciB4tfRTqs6IS2",
+      displayName: "kevboah"
+    ),
+    description: "Reactive training for the experienced Powerlifter. Get these gains the right way.",
+    pathToMedia: "assets/photos/deadlift.jpg",
+    trainingBlocks: []
+  ),
+  
+  // ProgramTemplate(
+  //   title: "Test Program 7"
+  // ),
+  // ProgramTemplate(
+  //   title: "Test Program 8"
+  // ),
+  // ProgramTemplate(
+  //   title: "Test Program 9"
+  // ),
+  // ProgramTemplate(
+  //   title: "Test Program 10"
+  // ),
+];
