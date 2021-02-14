3094b8cc1b7ccde1d9efd9f72a829bdc20889bc4
Kevin Yeboah <kevboah@gmail.com>
2021-02-13 22:53:34 -0500

    test create program button

--- lib/pages/app/profile/profile.dart
+++ lib/pages/app/profile/profile.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:juma/services/authService.dart';
+import 'package:juma/services/googleSheetsService.dart';
 import 'package:provider/provider.dart';
 import 'package:juma/models/users/user.dart';
 
@@ -26,11 +28,41 @@ class _ProfileState extends State<Profile> {
       body: Center(child: Text('PROFILE: ${user.displayName}'),),
 
       drawer: Drawer(
-        child: FlatButton(
-          child: Text('Sign Out'),
-          onPressed: () {
-            AuthService().signOut();
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              child: Text('Sign Out'),
+              onPressed: () {
+                AuthService().signOut();
+              },
+            ),
+            FlatButton(
+              child: Text('CREATE PROGRAM TEST'),
+              onPressed: () {
+                GoogleSheetsService gsService = GoogleSheetsService(); // google sheet service for creating the spreadhseet and stuff
+                var firebaseUser = user.firebase;
+
+                if (firebaseUser == null) {
+                  print("NULL firebase User!!");
+                  return;
+                }
+
+                UserInfo googleProvider = firebaseUser.providerData.firstWhere((provider) => provider.providerId == GoogleAuthProvider.providerId, 
+                  orElse: () => null);
+
+                // In this case we should have two pop ups? one for prompt to create program
+                // and one that says the user is not a google user and what to do next
+                if (googleProvider != null) {
+                  //gsService.createSpreadsheet(firebaseUser.email);
+                  print("Google user ${firebaseUser.email}");
+                }
+                else {
+                  print("NOT Google user ${firebaseUser.email}");
+                }
+              },
+            )
+          ],
         ),
       ),
     )
